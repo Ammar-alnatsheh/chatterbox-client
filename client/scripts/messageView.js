b35b3306e288450ca752330e8f200bd8139d9ba8
Ammar <ammar.alnatsheh@gmail.com>
2018-11-18 15:03:05 -0800

    buiild the message view style

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -3,7 +3,13 @@ var MessageView = {
   render: _.template(`
       <!--
       <div class="chat">
-        <div class="username"></div>
+        <div
+          class="username"
+          data-username="<%- username %>">
+          <%- username %>
+        </div>
+        <div><%- text %></div>
+          </div>
         <div></div>
       </div>
       -->
