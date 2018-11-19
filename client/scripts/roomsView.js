2f0ba8339f4d582434b9e708966b230e7c864b01
Ammar <ammar.alnatsheh@gmail.com>
2018-11-18 22:10:48 -0800

    add listener action to select room and add a new room

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,9 +4,30 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    RoomsView.$button.click(function() {
+      var room = prompt('Please enter a room name');
+      Rooms.add(room);
+      RoomsView.$select.val(room);
+    });
+
+    RoomsView.$select.change(function() {
+      Rooms.setSelectedRoom(RoomsView.$select.val());
+    });
+
   },
 
   render: function() {
+    var $rooms = Rooms.get();
+    $rooms.forEach(function(room) {
+      RoomsView.renderRoom(room);
+    });
+
+  },
+
+  renderRoom: function(room) {
+    var $option = $('<option>').val(room).text(room);
+    RoomsView.$select.append($option);
+
   }
 
 };
