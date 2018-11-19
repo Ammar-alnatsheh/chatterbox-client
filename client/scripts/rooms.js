428dcb0b1bc952f491bd235351987f6f65b4dc50
Ammar <ammar.alnatsheh@gmail.com>
2018-11-18 21:39:00 -0800

    build the rooms object to store them

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,4 +1,26 @@
 var Rooms = {
 
+  _list: new Set(),
+
+  update: function() {
+    Messages.get().forEach(function(message) {
+      Rooms._list.add(message.roomname);
+    });
+    // update the Rooms list
+    RoomsView.render();
+
+  },
+
+  get: function() {
+    return [...Rooms._list.values()].sort();
+
+  },
+
+  add: function(room) {
+    Rooms._list.add(room);
+    // update the Rooms list
+    RoomsView.render();
+
+  }
 
 };
