7aaf0d39c6d67e1c348d6c8b852bd650cfc22946
Ammar <ammar.alnatsheh@gmail.com>
2018-11-18 19:44:33 -0800

    sanitize each message before save it in storage

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -9,7 +9,7 @@ var Messages = {
   update: function(messages, callback = ()=>{}) {
 
     for (var i = 0; i < messages.length; i++) {
-      Messages._data[messages[i].objectId] = messages[i];
+      Messages._data[messages[i].objectId] = Messages.clean(messages[i]);
     }
 
     // only invoke the callback if something changed
@@ -19,6 +19,15 @@ var Messages = {
     }
     // after update the data show it on the screen
     MessagesView.render();
+  },
+
+  clean: function(message) {
+    // make sure every message hase a value for each key or replace with ''
+    // to keep the program running somooth
+    message.text = message.text || '';
+    message.username = message.username || '';
+    message.roomname = message.roomname || '';
+    return message;
   }
 
 };
