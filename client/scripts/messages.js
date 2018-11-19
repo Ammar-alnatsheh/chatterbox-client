1d13fe0abf3331f53c9f3b7544dacdb566563412
Ammar <ammar.alnatsheh@gmail.com>
2018-11-18 19:18:47 -0800

    update the data storage function and get function

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -2,28 +2,23 @@ var Messages = {
 
   _data: {},
 
-  items: function() {
+  get: function() {
       return _.chain(Object.values(Messages._data)).sortBy('createdAt');
   },
 
-  add: function(message, callback = ()=>{}) {
-    Messages._data[message.objectId] = message;
-    callback(Messages.items());
-  },
-
   update: function(messages, callback = ()=>{}) {
-    var length = Object.keys(Messages._data).length;
 
-    for (let message of messages) {
-      Messages._data[message.objectId] = Messages._conform(message);
+    for (var i = 0; i < messages.length; i++) {
+      Messages._data[messages[i].objectId] = messages[i];
     }
 
     // only invoke the callback if something changed
+    var length = Object.keys(Messages._data).length;
     if (Object.keys(Messages._data).length !== length) {
       callback(Messages.items());
     }
+    // after update the data show it on the screen
+    MessagesView.render();
   }
-
-  console.log(data);
 
 };
